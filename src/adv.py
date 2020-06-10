9471afc8f5f0fe42b0e66fe05b91bcd3efa5b516
Harry Merritt <harry@merritt.es>
2020-06-10 02:03:48 +0100

    create working REPL asking for cardinal direction

--- src/adv.py
+++ src/adv.py
@@ -1,22 +1,30 @@
+import os
+import textwrap
+
 from room import Room
+from player import Player
+
+# Clear console
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    "outside":  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    "foyer":    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    "overlook": Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    "narrow":   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    "treasure": Room("Treasure Chamber", """You"ve found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -24,20 +32,21 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room["outside"].n_to  = room["foyer"]
+room["foyer"].s_to    = room["outside"]
+room["foyer"].n_to    = room["overlook"]
+room["foyer"].e_to    = room["narrow"]
+room["overlook"].s_to = room["foyer"]
+room["narrow"].w_to   = room["foyer"]
+room["narrow"].n_to   = room["treasure"]
+room["treasure"].s_to = room["narrow"]
 
 #
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
+# Make a new player object that is currently in the "outside" room.
+player = Player("player1", room["outside"])
 
 # Write a loop that:
 #
@@ -46,6 +55,34 @@ room['treasure'].s_to = room['narrow']
 # * Waits for user input and decides what to do.
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
+# Print an error message if the movement isn"t allowed.
 #
 # If the user enters "q", quit the game.
+while True:
+    # Print current player location
+    print("Location:")
+    print(f"-- {player.current_room.name}")
+    print(f"{textwrap.fill(player.current_room.description, 40)}")
+
+    choice = input("\nWhere do you want to go? (#): ")
+    print()
+
+    try:
+        if choice == "q":
+            break
+
+        # Validate user input
+        # -> accept cardinal direction
+        if choice in ["n", "e", "s", "w"]:
+            # Check if room exists
+            if hasattr(player.current_room, f"{choice}_to"):
+                # Move player into room
+                player.current_room = getattr(player.current_room, f"{choice}_to")
+
+        else:
+            print("Not a valid cardinal direction")
+
+    except ValueError:
+        print("Please enter a valid number");
+
+    print("\n")
